f9222d1bf49ce84ff809a29809a946cd253258d4
jebus <gambawilliam77@gmail.com>
2023-10-07 12:23:08 +0200

    modification de l'image par l'action mouseover

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
 
-//const container = document.getElementsByTagName('main');
+
 const image = document.getElementById('img_1');
-const imagesArray = ["./resource/images/josh-hild-ttuS-GeWEao-unsplash.jpg","./resource/images/neom-gBCMAENwknA-unsplash.jpg","./resource/images/tanya-layko-ScLIyvFB13k-unsplash.jpg"];
+const arrayOfImages = ['https://images.unsplash.com/photo-1505533321630-975218a5f66f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80','https://images.unsplash.com/photo-1696456195495-1b420d4ebd3c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80','https://images.unsplash.com/photo-1505968409348-bd000797c92e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1471&q=80'];
 
-let randomImage = Math.floor(Math.random()* 2)
+function clickHandler(e) {
+    e.target.src = arrayOfImages[0];
+    setTimeout(()=> {
+      e.target.style.width = '300px';
+      e.target.style.height = '300px';
+      e.target.src = arrayOfImages[1];
+    },1000);
+    setTimeout(()=> {
+      e.target.src = arrayOfImages[2];
+    },2000);
+    setTimeout(()=> {
+      e.target.src = './resource/images/josh-hild-ttuS-GeWEao-unsplash.jpg';
+    },3000);
+}
 
-image.addEventListener('click',(e) => {
-    let imageSrc = e.target.src = imagesArray[randomImage]
-    image.value = imageSrc;
-})
+image.addEventListener('mouseover', clickHandler);
+
+
+
+
+
     
+
+  
    
     
 
