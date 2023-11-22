1bbee4ba901ce7bc1adda801e8967994b96ecc08
jebus <gambawilliam77@gmail.com>
2023-11-22 19:41:13 +0100

    the site is made responsive

--- script.js
+++ script.js
@@ -3,10 +3,10 @@
 const image = document.getElementById('img_1');
 const arrayOfImages = ['https://images.unsplash.com/photo-1505533321630-975218a5f66f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80','https://images.unsplash.com/photo-1696456195495-1b420d4ebd3c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80','https://images.unsplash.com/photo-1505968409348-bd000797c92e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1471&q=80'];
 
-function clickHandler(e) {
-    e.target.src = arrayOfImages[0];
+function overHandler(e) {
+    /*e.target.src = arrayOfImages[0];*/
     setTimeout(()=> {
-      e.target.style.width = '450px';
+      e.target.style.width = '100%';
       e.target.style.height = '300px';
       e.target.src = arrayOfImages[1];
     },1000);
@@ -18,7 +18,7 @@ function clickHandler(e) {
     },3000);
 }
 
-image.addEventListener('mouseover', clickHandler);
+image.addEventListener('mouseover', overHandler);
 
 
 
